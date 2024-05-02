fb02233279e76c4acf0174e57cc3d2927f37d2d1
mckent05 <atemitope95@gmail.com>
2024-05-02 17:11:37 +0100

    Added LinkedList ds

---
+++ LinkedList.js
@@ -0,0 +1,111 @@
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+
+    addNode(data) {
+        
+        if(this.head === null) {
+            this.head = new Node(data)
+        }
+        else {
+            let current = this.head
+            while(current.next !== null) {
+                current = current.next
+            }
+            current.next = new Node(data)
+        }
+        this.length++
+    }
+
+    insertAtIndex(index, data) {
+        let current = this.head
+        let previous;
+        let node = new Node(data)
+        if( index === 0) {
+            if (current === null) {
+                this.head = new Node(data)
+            }
+            else {
+                this.head = node
+                node.next = current
+            }
+            
+        }
+        else {
+            let count = 0
+            while(count < index) {
+                previous = current
+                current = current.next
+                count++
+            }
+            previous.next = node
+            node.next = current
+            
+        }
+        this.length++
+    }
+    replaceAtIndex(index, data) {
+        let current = this.head;
+        let node = new Node(data)
+        if(index > this.length) {
+            console.log("Wrong index please")
+            return;
+        } 
+        if(index === 0) {
+            if(current === null) {
+                this.head = node
+            } else {
+                current.data = data
+            }
+        }
+        else {
+            let count = 0
+            while(count < index) {
+                current = current.next
+                count++
+            }
+            current.data = data
+        }
+    }
+
+    getDataAtIndex(index){
+        let count = 0
+        let current = this.head
+        while(count < index){
+            current = current.next
+            count++
+        }
+        console.log(current.data)
+    }
+
+    deleteDataAtIndex(index){
+        let count = 0
+        let current = this.head
+        if(index === 0) {
+            if(current === null) {
+                return;
+            }
+            this.head = current.next
+        }
+        else {
+            let previous;
+            while(count < index) {
+                previous = current
+                current = current.next
+                count++
+            }
+            previous.next = current.next
+        }
+        this.length--
+    }
+    
+}
