f00774158b472f8508f0b87b717626208231e4c7
mckent05 <atemitope95@gmail.com>
2024-05-17 23:38:20 +0100

    Added solution for minimum island

--- Graphs.js
+++ Graphs.js
@@ -1,32 +1,47 @@
-const graph = {
-    a: ["b", "c", "d"],
-    b: ["a", "c"],
-    c: ["a", "d"],
-    d: ["a", "c"],
-    e: ["f"],
-    f: ["e"],
-    g: []
-}
+const graph = [
+    ["W", "L", "L", "W"],
+    ["W", "L", "W", "L"],
+    ["W", "L", "W", "L"],
+    ["W", "W", "L", "W"],
+    ["W", "L", "L", "W"],
+    ["W", "W", "L", "W"],
+]
 
 const graphImplement = (graph) => {
     let visited = new Set()
     let myArray = []
     let size;
-    for(let node in graph) {
-        size = navigateGraph(graph, node, visited)
-        if(size) {
-            myArray.push(size)
+    for(let i = 0; i < graph.length; i++) {
+        for(let j = 0; j < graph[0].length; j++){
+            size = navigateGraph(graph, i, j, visited)
+            if(size) {
+                myArray.push(size)
+            }
         }
     }
+    // for(let node in graph) {
+    //     size = navigateGraph(graph, node, visited)
+    //     if(size) {
+    //         myArray.push(size)
+    //     }
+    // }
     return myArray
 }
 
-const navigateGraph = (graph, current, visited, countNode=1) => {
-    if(visited.has(current)) return 0
-    visited.add(current)
-    for(let neigbors of graph[current]) {
-        navigateGraph(graph, neigbors, visited, countNode++)
-    }
+const navigateGraph = (graph, row, col, visited, countNode=1) => {
+    const rowBound = 0 <= row && row < graph.length
+    const colBound = 0 <= col && col < graph[0].length
+    if(!rowBound || !colBound) return 0
+
+    if(graph[row][col] === "W") return 0
+
+    const pos = `${row},${col}`
+    if(visited.has(pos)) return 0
+    visited.add(pos)
+    navigateGraph(graph, row +1, col, visited, countNode+=1)
+    navigateGraph(graph, row-1, col, visited, countNode+=1)
+    navigateGraph(graph, row, col +1, visited, countNode+=1)
+    navigateGraph(graph, row, col-1, visited, countNode+=1)
     return countNode
 
 }
