58705aea2821fa4f547e7e2caedfb61e9af59974
mckent05 <atemitope95@gmail.com>
2024-06-05 14:57:06 +0100

    added solution for leaderboad solution hackerrank

---
+++ Hackerrank/Leaderboard.js
@@ -0,0 +1,13 @@
+const leaderBoard = (rank, player) => {
+   
+}
+
+const findLeaderBoard = (rank, score, currentIndex, result) => {
+    if(!(currentIndex >=0 && score >= rank[currentIndex])) {
+        result.push(currentIndex + 2)
+        return
+    }
+    return findLeaderBoard(rank, score, currentIndex-1, result)
+}
+
+leaderBoard([100,90,90,80], [70,80,105])
